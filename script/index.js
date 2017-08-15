8bb83b2145622c94b0bb7b42f8ef2d3788fc99bd
Dwire <gjdwyer8@gmail.com>
2017-08-15 15:35:50 -0700

    added js timeout function to enable new css transition upon video completion

--- script/index.js
+++ script/index.js
@@ -1,1 +1,9 @@
-// fun
+const container = document.querySelector('.sectionsContainer');
+
+
+function addClass() {
+    container.classList.toggle("visible-options");
+    container.classList.toggle("visibility-none");
+}
+
+setTimeout(addClass, 20500);
